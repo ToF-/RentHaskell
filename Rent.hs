d33217121f846b4f863932115afd1904574de458
ToF- <cthibaut@octo.com>
2013-10-19 15:35:30 +0200

    extract of lengths and order data in group of orders

--- Rent.hs
+++ Rent.hs
@@ -1,9 +1,33 @@
 module Rent
 where
 
-type Order = (Int, Int, Int) 
+type Time = Int
+type Duration = Int
+type Price = Int
+type Order = (Time, Duration, Price) 
 
 process :: ([Order] -> Int) -> String -> String
 process _ ('1':_) = "18"
 process _ ('2':_) = "10\r8"
+
+extract :: [[Int]] -> [[Order]]
+extract ns = let
+    lengths = concat $ filter isLength ns
+    orders  = map fromInts $ filter isOrder ns
+    in extract' lengths orders 
+
+isLength :: [Int] -> Bool
+isLength = (1 ==).length
+
+isOrder :: [Int] -> Bool
+isOrder = (3 ==).length
+
+fromInts :: [Int] -> Order
+fromInts [s,d,p] = (s,d,p)
+
+extract' :: [Int] -> [Order] -> [[Order]]
+extract' [] _  = []
+extract' (n:ns) os = (take n os) : extract' ns (drop n os) 
+
+
 
--- Specs.hs
+++ Specs.hs
@@ -11,6 +11,10 @@ main = hspec $ do
         it "scan several cases and give several solutions" $ do
             process mockOptimizer "2\r1\r0 5 10\r2\r5 9 7\r6 9 8" `shouldBe` "10\r8"
 
+    describe "extract" $ do
+        it "forms a list of lists of orders" $ do
+            extract [[1],[0,5,10],[2],[3,7,9],[5,9,8]] `shouldBe` [[(0,5,10)],[(3,7,9),(5,9,8)]] 
+
 mockOptimizer :: [Order] -> Int
 mockOptimizer os = case length os of
                     4 -> 18
