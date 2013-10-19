7c34bf0abf240aab56d08059aaa2cda0dbabb17a
ToF- <cthibaut@octo.com>
2013-10-19 14:59:42 +0200

    extract the optimizer method, fulfilled by a mock for now

--- Rent.hs
+++ Rent.hs
@@ -1,7 +1,9 @@
 module Rent
 where
 
-process :: String -> String
-process ('1':_) = "18"
-process ('2':_) = "10\r8"
+type Order = (Int, Int, Int) 
+
+process :: ([Order] -> Int) -> String -> String
+process _ ('1':_) = "18"
+process _ ('2':_) = "10\r8"
 
--- Specs.hs
+++ Specs.hs
@@ -6,7 +6,13 @@ main :: IO ()
 main = hspec $ do
     describe "process" $ do
         it "scan input for a case with orders and output a solution" $ do
-            process "1\r4\r0 5 10\r3 7 14\r5 9 7\r6 9 8" `shouldBe` "18"
+            process mockOptimizer "1\r4\r0 5 10\r3 7 14\r5 9 7\r6 9 8" `shouldBe` "18"
 
         it "scan several cases and give several solutions" $ do
-            process "2\r1\r0 5 10\r2\r5 9 7\r6 9 8" `shouldBe` "10\r8"
+            process mockOptimizer "2\r1\r0 5 10\r2\r5 9 7\r6 9 8" `shouldBe` "10\r8"
+
+mockOptimizer :: [Order] -> Int
+mockOptimizer os = case length os of
+                    4 -> 18
+                    2 -> 8
+                    1 -> 10  
