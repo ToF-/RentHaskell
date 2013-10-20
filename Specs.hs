1e53d597d159fcdd983c683e6f70e750860d7182
ToF- <cthibaut@octo.com>
2013-10-20 11:59:57 +0200

    edited description with type signature

--- Specs.hs
+++ Specs.hs
@@ -5,18 +5,18 @@ import Rent
 
 main :: IO ()
 main = hspec $ do
-    describe "process" $ do
+    describe "process :: String -> String" $ do
         it "scan input for a case with orders and output a solution" $ do
             process mockOptimizer "1\n4\n0 5 10\n3 7 14\n5 9 7\n6 9 8" `shouldBe` "18\n"
 
         it "scan several cases and give several solutions" $ do
             process mockOptimizer "2\n1\n0 5 10\n2\n5 9 7\n6 9 8" `shouldBe` "10\n8\n"
 
-    describe "extract" $ do
+    describe "extract :: [[Int]] -> [[Order]]" $ do
         it "forms a list of lists of orders" $ do
             extract [[1],[0,5,10],[2],[3,7,9],[5,9,8]] `shouldBe` [[(0,5,10)],[(3,7,9),(5,9,8)]] 
 
-    describe "optimize (list)" $ do
+    describe "optimizeL :: [Order] -> Int -- (list algorithm)" $ do
         it "when given 1 order gives the order price" $ property $
             forAll order $ \o -> optimizeL [o] == price o 
     
