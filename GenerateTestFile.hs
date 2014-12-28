e8e834176b69b627d25a55619acd2c8ab33ac22f
ToF- <cthibaut@octo.com>
2014-12-28 12:37:20 +0100

    refactoring

--- GenerateTestFile.hs
+++ GenerateTestFile.hs
@@ -1,5 +1,6 @@
 import System.Random (getStdRandom, randomR)
 import System.Environment (getArgs)
+import Control.Monad (replicateM)
 
 {-- runghc GenerateTestFile M 
  -      generates 30 test cases of 10000 orders
@@ -12,17 +13,19 @@ main = do
     d <- randomTestData (args == ["M"])
     putStr $ unlines $ showTestData d
 
-randomInt :: (Int, Int) -> IO Int
-randomInt = getStdRandom . randomR
-
-randomTime = randomInt (0, 1000000)
-randomPrice = randomInt (1, 100000)
 {--
 The start time of the order st (0 ≤ st < 1000000),
 the duration d of the order (0 < d < 1000000),
 and the price p (0 < p < 100000) the customer
 is ready to pay for this order.
- --}
+--}
+
+randomInt :: (Int, Int) -> IO Int
+randomInt = getStdRandom . randomR
+
+randomTime = randomInt (0, 1000000)
+randomPrice = randomInt (1, 100000)
+
 type Order = (Int, Int, Int)
 type Problem = (Int, [Order])
 type TestData = (Int, [Problem])
@@ -37,13 +40,13 @@ randomOrder = do
 randomProblem :: Bool -> IO Problem
 randomProblem maxed = do
     n <- if maxed then return 10000 else randomInt (1,10001)
-    os <- sequence $ replicate n randomOrder
+    os <- replicateM n randomOrder
     return (n,os)
 
 randomTestData :: Bool -> IO TestData
 randomTestData maxed = do 
     n <- if maxed then return 30 else randomInt (1,31)
-    ps <- sequence $ replicate n $ randomProblem maxed
+    ps <- replicateM n $ randomProblem maxed
     return (n,ps)
 
 showOrder :: Order -> String
