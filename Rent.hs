783fc746aa7dee769b76a487fae2caa3c7e396f4
ToF- <cthibaut@octo.com>
2014-12-28 15:50:40 +0100

    Order type

--- Rent.hs
+++ Rent.hs
@@ -1,6 +1,6 @@
 import Test.Hspec
 
-testing = False 
+testing = True 
 -- True: pass tests. False: run program
 -- passing an arg won't do because hspec wants to consume the arg too
 
@@ -11,6 +11,16 @@ process = putStrLn "18"
 
 tests :: IO ()
 tests = hspec $ do
-    describe "test" $ do
-        it "should pass" $ do
-            pendingWith "to do"
+    describe "an order" $ do
+        it "should have a start time" $ do
+            startTime (makeOrder 3 5 10) `shouldBe` 3
+        it "should have an end time" $ do
+            endTime (makeOrder 3 5 10) `shouldBe` 3+5
+        it "should have a price" $ do
+            price (makeOrder 3 5 10) `shouldBe` 10
+
+data Order = Order { startTime :: Int, endTime :: Int, price :: Int }
+
+makeOrder :: Int -> Int -> Int -> Order
+makeOrder s d p = Order s (s+d) p 
+
