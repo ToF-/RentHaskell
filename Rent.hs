14c81ae125bc06d5d1b75f34436ad67dd927ad1e
ToF- <cthibaut@octo.com>
2014-12-29 10:52:42 +0100

    running version

--- Rent.hs
+++ Rent.hs
@@ -3,14 +3,11 @@ import Data.List (sort)
 import Data.Map as Map (Map,insertWith,empty,(!),insert,elems,keys,findWithDefault,findMax)
 import qualified Data.Map as Map 
 
-testing = True 
+testing = False 
 -- True: pass tests. False: run program
 -- passing an arg won't do because hspec wants to consume the arg too
 
-main = if testing then tests else process
-
-process :: IO ()
-process = putStrLn "18"
+main = if testing then tests else interact process
 
 os = [makeOrder 0 5 10
      ,makeOrder 3 7 14
@@ -53,7 +50,24 @@ tests = hspec $ do
                      ,makeOrder 5 9 7
                      ,makeOrder 6 9 8]        
             profit os `shouldBe` 18
-    
+   
+    describe "solve" $ do
+        it "solves a series of profit problems" $ do 
+            let xs = [[4]
+                     ,[0,5,10]
+                     ,[3,7,14]
+                     ,[5,9,7 ]
+                     ,[6,9,8 ]
+                     ,[3]
+                     ,[0,5,12]
+                     ,[3,7,14]
+                     ,[5,6,10]]
+            solve xs `shouldBe` [18,22]
+
+    describe "process" $ do
+        it "converts problem input into solution output" $ do
+            let s = "2\n4\n0 5 10\n3 7 14\n5 9 7 \n6 9 8 \n3\n0 5 12\n3 7 14\n5 6 10"
+            process s `shouldBe` "18\n22\n"
 
 type Time  = Int
 type Money = Int
@@ -63,6 +77,15 @@ data Order = Order { startTime :: Time,
     deriving (Eq, Ord, Show)
 type Plan    = Map Time [Order] 
 type Profits = Map Time Money
+
+process :: String -> String
+process  = unlines . map show . solve . tail . map (map read) . map words . lines  
+
+solve :: [[Int]] -> [Int]
+solve [] = []
+solve ([n]:xs) = solveProblem (take n xs) : solve (drop n xs)
+    where
+    solveProblem = profit . map (\[s,d,p] -> makeOrder s d p) 
 
 profit :: [Order] -> Money
 profit = money . findMax . exploit . plan
